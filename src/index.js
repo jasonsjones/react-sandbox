44999de6706a3352aee8498225ea26ce833624ca
Jason <jasonjones@salesforce.com>
2018-02-13 16:03:06 -0800

    Add App functional cmp

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'sldsCss';
 
+const App = () => {
+    return (
+        <div style={{ width: '960px', margin: '50px auto'}}>
+            <h1 className="slds-text-heading_large">Hello React</h1>
+        </div>
+    );
+};
+
 ReactDOM.render(
-    <h1>Hello React</h1>,
+    <App />,
     document.getElementById('app')
 );
