b695af55a64d65ea30dfb837297781f3aa961e13
Jason <jasonjones@salesforce.com>
2018-02-18 11:53:22 -0800

    Add App component

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import 'sldsImages/logo.svg';
+import 'sldsCss';
+
+const GlobalHeader = () => {
+    return (
+        <header className="slds-global-header_container">
+            <a href="javascript:void(0);" className="slds-assistive-text slds-assistive-text_focus">Skip to Navigation</a>
+            <a href="javascript:void(0);" className="slds-assistive-text slds-assistive-text_focus">Skip to Main Content</a>
+            <div className="slds-global-header slds-grid slds-grid_align-spread">
+                <div className="slds-global-header__item">
+                    <div className="slds-global-header__logo">
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+            <GlobalHeader />
+            <div style={{ width: '960px', margin: '5rem auto 0'}}>
+                <h1 className="slds-text-heading_large">Hello React Sandbox!</h1>
+            </div>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'sldsCss';
 
-const App = () => {
-    return (
-        <div style={{ width: '960px', margin: '50px auto'}}>
-            <h1 className="slds-text-heading_large">Hello React</h1>
-        </div>
-    );
-};
+import App from './components/App';
 
 ReactDOM.render(
     <App />,
