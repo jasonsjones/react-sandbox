a08f6c75a6737472446ac23c52d3a42476e5a437
Jason <jasonjones@salesforce.com>
2018-03-25 11:59:42 -0700

    Add GlobalHeader component

--- src/components/OneAppPage/OneAppPage.js
+++ src/components/OneAppPage/OneAppPage.js
@@ -1,22 +1,6 @@
 import React from 'react';
 
-import 'sldsCss/salesforce-lightning-design-system.min.css';
-import 'sldsImages/logo.svg';
-
-const GlobalHeader = () => {
-    return (
-        <header className="slds-global-header_container">
-            <a href="javascript:void(0);" className="slds-assistive-text slds-assistive-text_focus">Skip to Navigation</a>
-            <a href="javascript:void(0);" className="slds-assistive-text slds-assistive-text_focus">Skip to Main Content</a>
-            <div className="slds-global-header slds-grid slds-grid_align-spread">
-                <div className="slds-global-header__item">
-                    <div className="slds-global-header__logo">
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
-};
+import GlobalHeader from '../GlobalHeader';
 
 const OneAppPage = () => {
     return (
