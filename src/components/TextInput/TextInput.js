a33d355ff68e625549d09bc9d9db6e095c0a1227
Jason <jasonjones@salesforce.com>
2018-03-30 14:59:49 -0700

    Add name and handleChange props

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -12,8 +12,9 @@ const TextInput = (props) => {
         <div className="slds-form-element">
             <label className="slds-form-element__label" htmlFor={props.name}>{props.label}</label>
             <div className="slds-form-element__control">
-                <input type={props.type} id={props.name} name={props.name}
-                    className="slds-input" style={ styles[props.size] } placeholder={props.placeholder} />
+                <input type={props.type} id={props.name} name={props.name} value={props.value}
+                    onChange={props.handleChange} className="slds-input" style={styles[props.size]}
+                    placeholder={props.placeholder} />
             </div>
         </div>
     );
@@ -21,10 +22,12 @@ const TextInput = (props) => {
 
 TextInput.propTypes = {
     name: PropTypes.string,
+    value: PropTypes.string,
     label: PropTypes.string,
     type: PropTypes.string,
     size: PropTypes.string,
-    placeholder: PropTypes.string
+    placeholder: PropTypes.string,
+    handleChange: PropTypes.func
 };
 
 export default TextInput;
