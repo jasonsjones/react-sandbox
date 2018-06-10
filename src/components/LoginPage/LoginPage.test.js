3b02a8183690da3196b62e20928e0e6d6a6c5136
Jason Jones <jsjones96@gmail.com>
2018-06-10 14:33:58 -0700

    Update tests

--- src/components/LoginPage/LoginPage.test.js
+++ src/components/LoginPage/LoginPage.test.js
@@ -9,17 +9,4 @@ describe('LoginPage component', () => {
         const wrapper = shallow(<LoginPage/>);
         expect(wrapper).toBeDefined;
     });
-
-    it('renders outer div with slds-grid', () => {
-        const wrapper = shallow(<LoginPage/>);
-        expect(wrapper.find('div').first().hasClass('slds-grid')).toBeTrue;
-    });
-
-    it.skip('renders inner divs with slds-size_1-of-2', () => {
-        const wrapper = shallow(<LoginPage/>);
-        let children = wrapper.find('div').first().children();
-        expect(children).toHaveLength(2);
-        expect(children.first().hasClass('slds-size_1-of-2')).toBeTrue;
-        expect(children.last().hasClass('slds-size_1-of-2')).toBeTrue;
-    });
 });
