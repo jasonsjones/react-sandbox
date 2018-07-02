e17abeeb407a7beac807036272d691a59a8c1d8c
Jason Jones <jsjones96@gmail.com>
2018-07-02 08:52:19 -0700

    Refactor LoginPage for better testing

--- src/containers/LoginPage/LoginPage.js
+++ src/containers/LoginPage/LoginPage.js
@@ -7,19 +7,19 @@ import LoginForm from '../../components/LoginForm';
 import 'sldsImages/logo-noname.svg';
 import logo from 'sldsImages/logo.svg';
 
-const LeftSide = () => {
-    const loginFormStyles = {
-        width: '400px',
-        margin: '0 auto',
-        padding: '2rem',
-        border: '1px solid #ccc',
-        borderRadius: '8px'
-    };
+const loginFormStyles = {
+    width: '400px',
+    margin: '0 auto',
+    padding: '2rem',
+    border: '1px solid #ccc',
+    borderRadius: '8px'
+};
 
-    const imageStyles = {
-        width: '50%'
-    };
+const imageStyles = {
+    width: '50%'
+};
 
+const renderLeftSide = () => {
     return (
         <section style={{ width: '400px', margin: '25% auto' }}>
             <div className="slds-grid slds-grid_vertical slds-grid_vertical-align-center">
@@ -36,9 +36,7 @@ const LeftSide = () => {
 
 const renderLoginPage = () => (
     <div className="slds-grid slds-grid_frame">
-        <div className="slds-size_1-of-2 slds-p-horizontal_medium">
-            <LeftSide />
-        </div>
+        <div className="slds-size_1-of-2 slds-p-horizontal_medium">{renderLeftSide()}</div>
         <div
             className="slds-size_1-of-2 slds-p-horizontal_medium"
             style={{ backgroundColor: '#efefef' }}
