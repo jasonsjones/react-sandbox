27699f8673c35a26f9bf740e74380ce52dbebe47
Jason Jones <jsjones96@gmail.com>
2018-07-04 10:06:41 -0700

    Add test for LoginPage

--- src/containers/LoginPage/LoginPage.test.js
+++ src/containers/LoginPage/LoginPage.test.js
@@ -3,10 +3,22 @@ import { shallow } from 'enzyme';
 
 import LoginPage from './LoginPage';
 
-describe('LoginPage component', () => {
+const createComponent = isAuth => {
+    const outer = shallow(<LoginPage />);
+    const Children = outer.props().children({ isAuth });
+    return shallow(Children);
+};
 
+describe('LoginPage component', () => {
     it('instantiates the LoginPage component', () => {
-        const wrapper = shallow(<LoginPage/>);
+        const wrapper = createComponent(false);
         expect(wrapper).toBeDefined;
     });
+
+    it('renders the login form if the user is not authenticated', () => {
+        const wrapper = createComponent(false);
+        expect(wrapper.find('LoginForm')).toHaveLength(1);
+    });
+
+    it('renders a Redirect if the user is authenticated');
 });
