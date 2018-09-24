49714bb2f8dd352ccec104d1ab5727b3c571ecea
Jason Jones <jsjones96@gmail.com>
2018-09-24 12:12:22 -0700

    Update content on page (fetch from api)

--- src/containers/OneAppPage/OneAppPage.js
+++ src/containers/OneAppPage/OneAppPage.js
@@ -4,16 +4,66 @@ import { connect } from 'react-redux';
 
 import GlobalHeader from '../../components/GlobalHeader';
 
-export const OneAppPage = props => {
-    return (
-        <div>
-            <GlobalHeader />
-            <div style={{ width: '960px', margin: '5rem auto 0' }}>
-                <h1 className="slds-text-heading_large">Hello {props.contextUser.name}</h1>
+export class OneAppPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: '',
+            version: '',
+            error: null
+        };
+    }
+    componentDidMount() {
+        fetch('http://localhost:3000/api')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    message: data.message,
+                    version: data.version,
+                    error: null
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({
+                    error: err.message
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <GlobalHeader />
+                <div style={{ width: '960px', margin: '5rem auto 0' }}>
+                    <h1 className="slds-text-heading_large">Hello {this.props.contextUser.name}</h1>
+                    {!this.state.error && (
+                        <div>
+                            <h1
+                                style={{ marginTop: '30px' }}
+                                className="slds-text-heading_large slds-text-color_weak"
+                            >
+                                Message: {this.state.message}
+                            </h1>
+                            <h1 className="slds-text-heading_large slds-text-color_weak">
+                                Version: {this.state.version}
+                            </h1>
+                        </div>
+                    )}
+                    {this.state.error && (
+                        <h3
+                            style={{ marginTop: '50px' }}
+                            className="slds-text-heading_medium slds-text-color_error"
+                        >
+                            {this.state.error}
+                        </h3>
+                    )}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 OneAppPage.propTypes = {
     contextUser: PropTypes.object
